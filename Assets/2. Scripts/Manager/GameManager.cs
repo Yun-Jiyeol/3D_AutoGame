d4eb6b3177f5b68c7bc95ca271e86dfb0338c0d1
Yun-Jiyeol <yir0612@gmail.com>
2025-03-24 12:32:59 +0900

    [Feat] AI Nav 설정

--- Assets/2. Scripts/Manager/GameManager.cs
+++ Assets/2. Scripts/Manager/GameManager.cs
@@ -6,12 +6,19 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
+    [Header("Player")]
+    public Player player;
+
     [Header("Enemy")]
     public List<GameObject> DragonPrefab;
     public List<GameObject> EnemyPrefab;
     public MonsterStat normalStat;
     public MonsterStat minibossStat;
     public MonsterStat bossStat;
+
+    private Vector3 normalmonsterSize = new Vector3(3, 3, 3);
+    private Vector3 minibossmonsterSize = new Vector3(5, 5, 5);
+    private Vector3 bossmonsterSize = new Vector3(1.5f, 1.5f, 1.5f);
 
     [Header("Position")]
     public List<GameObject> monsterSpawnPosition;
